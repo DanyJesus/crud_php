8b04863742d0e76ca8e880d0ec805edd61352960
Paulo.jesus <paulohenrique.nj@gmail.com>
2021-01-28 21:08:02 -0300

    add files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # crud_php
+
+O objetivo é criar algumas funções que irão alterar o sqlite que está no diretório database.
+
+Tem um arquivo de test que indicará se as açoes basicas estão sendo realizadas.
+
+Quando todos os testes passarem, são bem poucos, chegamos no final.
+
+
+1. Clone o projeto
+2. Instale as dependências (É necessário ter o composer instalado)  
+`composer install`
+3. Rode os testes, no diretório onde está o clone do projeto rode no terminal  
+`vendor/bin/phpunit`
+
+> OBS: Use as mensagens de erro que vão aparcer ao rodar os teste para se orientar em relação ao que precisa ser feito. Se estiver muito complicado, nos avise. =D
+
+> Se você estiver animad[oa] crie outros testes e funções, só tem o básico do básico ai, ainda dá para criar bastante coisa.
+
---
+++ tests/CrudTest.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use CRUD\Crud;
+
+/**
+ * Vamos testar as funções da classe Crud.
+ */
+final class CrudTest extends TestCase
+{
+
+
+    public function testInsert(): void
+    {
+        $id = $this->insert();
+
+        $this->assertIsInt($id);
+    }
+
+
+    public function testUpdate(): void
+    {
+        $crud = new Crud();
+
+        $id = $this->insert();
+
+        $up = $crud->update($id, ['symbol' => 'HH']);
+
+        $this->assertEquals($up, true);
+    }
+
+    public function testDelete(): void
+    {
+        $crud = new Crud();
+
+        $id = $this->insert();
+
+        $del = $crud->delete($id);
+
+        $this->assertEquals($del, true);
+    }
+
+    public function testList(): void
+    {
+        $crud = new Crud();
+
+        $list = $crud->listAll();
+
+        $this->assertNotEmpty($list);
+    }
+
+    public function testListOne(): void
+    {
+        $crud = new Crud();
+
+        $id = $this->insert();
+
+        $list = $crud->listOne($id);
+
+        $this->assertCount(1, $list);
+    }
+
+    private function insert()
+    {
+        $crud = new Crud();
+        return $crud->insert(
+            'Hydrogen',
+            'H',
+            1,
+            1
+        );
+    }
+}
